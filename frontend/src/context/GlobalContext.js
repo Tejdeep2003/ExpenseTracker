6ce46fe32689c6e11b3362b77053b0caef4ead96
Tejdeep2003 <tejdeepgutta03@gmail.com>
2023-12-01 19:00:56 +0530

    Frontend Working Income updated

--- frontend/src/context/GlobalContext.js
+++ frontend/src/context/GlobalContext.js
@@ -17,26 +17,12 @@ export const GlobalProvider = ({children}) => {
             .catch((err) =>{
                 setError(err.response.data.message)
             })
-        getIncomes()
-    }
-
-    const getIncomes = async () => {
-        const response = await axios.get(`${BASE_URL}get-incomes`)
-        setIncomes(response.data)
-        console.log(response.data)
-    }
-
-    const deleteIncome = async (id) => {
-        const res  = await axios.delete(`${BASE_URL}delete-income/${id}`)
-        getIncomes()
-    }
+        }
     
 
     return (
         <GlobalContext.Provider value={{
             addIncome,
-            getIncomes,
-            deleteIncome
         }}>
             {children}
         </GlobalContext.Provider>
